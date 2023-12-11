e46acbbe51fd99c55c37b14a818833f82b3535a5
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2023-12-11 16:10:40 +0530

    href instead of routerLink

--- src/app/components/second-page/second-page.component.ts
+++ src/app/components/second-page/second-page.component.ts
@@ -1,16 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-second-page',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterLink
-  ],
+  imports: [CommonModule],
   templateUrl: './second-page.component.html',
   styleUrl: './second-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SecondPageComponent { }
+export class SecondPageComponent {}
