85a91260f6356b3757cdb75f9663572352ccc7d1
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2023-12-11 09:55:32 +0530

    route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,4 @@ import { HomeComponent, SecondPageComponent } from './components';
 export const routes: Routes = [
   { path: 'page2', component: SecondPageComponent },
   { path: '', pathMatch: 'full', component: HomeComponent },
-  { path: '**', pathMatch: 'full', redirectTo: '' },
 ];
