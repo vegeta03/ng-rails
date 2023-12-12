89a4da19c6cf013b84ae63d5c4ae130d70434144
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2023-12-12 18:19:23 +0530

    RouterLink

--- src/app/components/second-page/second-page.component.ts
+++ src/app/components/second-page/second-page.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-second-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './second-page.component.html',
   styleUrl: './second-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
