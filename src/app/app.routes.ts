3a3e8282b3ffd0558d58a68b47bcbf71dee201a2
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2023-12-29 23:34:17 +0530

    new components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,16 @@
 import { Routes } from '@angular/router';
 
 import {
-  HomeComponent,
+  Page1Component,
+  Page2Component,
+  Page4Component,
   RedirectToRailsComponent,
-  SecondPageComponent,
 } from './components';
 
 export const routes: Routes = [
-  { path: 'page2', component: SecondPageComponent },
-  { path: '', pathMatch: 'full', component: HomeComponent },
+  { path: 'page1', component: Page1Component },
+  { path: 'page2', component: Page2Component },
+  { path: 'page4', component: Page4Component },
+  { path: '', pathMatch: 'full', component: RedirectToRailsComponent },
   { path: '**', pathMatch: 'full', component: RedirectToRailsComponent },
 ];
