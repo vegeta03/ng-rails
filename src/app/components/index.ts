2cac32dcd8cb24c08f263af52cdb4d6916f1d276
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2024-01-08 11:43:39 +0530

    c;lean

--- src/app/components/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './page1/page1.component';
-export * from './page2/page2.component';
-export * from './page4/page4.component';
-export * from './redirect-to-rails/redirect-to-rails.component';
